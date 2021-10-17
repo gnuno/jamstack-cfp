1cc012e2f855cd6d6c9c5531f1897c06125b48a2
Phosphorus Moscu <fernando.n.p.d.moscu@gmail.com>
2021-10-16 23:02:52 -0300

    Update Submission.vue

--- src/components/Submission.vue
+++ src/components/Submission.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="py-5 border-t-2 border-gray-200">
     <div class="block">
-      <span v-html="issue.html" class="block mt-3 text-base leading-6 text-gray-600"
-        ></span
-      >
+      <span v-html="issue.html" class="block mt-3 text-base leading-6 text-gray-600"></span>
     </div>
   </div>
 </template>
